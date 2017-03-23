f9537cb4004e9f2f839a7d201498080251cf8c70
tatmush <tatalmondmush@gmail.com>
2017-03-23 11:43:55 +0200

    first commit, calculating mersenne primes. evaluate code to add efficiency(optimization)

---
+++ primeNumbers.py
@@ -0,0 +1,27 @@
+from math import *
+
+#tests if a number is prime or not, if it
+#is then that number is returned else 0
+def testPrime(testCase):
+
+    #loop through all the numbers until you get to math.ceil(x/2)
+    betterTest = ceil(testCase/2)
+    for y in range(3, betterTest, 2):
+        if (testCase % y) == 0:
+            return 0
+        elif (testCase % y) != 0:
+            continue
+    return testCase
+            
+#look for mersenne prime numbers
+for x in range(3, 999999, 2):
+    y = testPrime(x)
+    if y != 0:
+        print("testing %d" % y)
+        power = (2**y) - 1
+        
+        z = testPrime(power)
+        if z != 0:
+            print("I have found a merssene prime %d" % power)
+            print("Formed after %d" % y)
+
